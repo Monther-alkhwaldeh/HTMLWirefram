088ec659a1d8e62d8bb78d068e5d00bd624d12b9
Monther-Alkhwaldeh <mmskbh.91@gmail.com>
2021-01-27 15:45:31 +0200

    add java scribt

---
+++ jspage.js
@@ -0,0 +1,24 @@
+var typeoflaptob=prompt("which type of laptob do you want");
+var nooflaptob=prompt("how many laptob do you want");
+
+var orderdetails;
+
+if(nooflaptob >1 && nooflaptob <=3)
+{
+orderdetails="you get 10% discount";
+} else if(nooflaptob >=4 && nooflaptob <= 8){
+    orderdetails="you get 20% discount";
+} else if (nooflaptob >= 9 && nooflaptob <= 20){
+    orderdetails="you git 35% discount ";
+} else {
+    orderdetails="sorry no discount ";
+}
+
+
+document.write(orderdetails);
+document.getElementById("order").innerText=typeoflaptob;
+document.getElementById("nolaptob").innerText=nooflaptob;
+
+confirm("Are you happy with you discount ");
+alert("thank you for shopping from our shop");
+
