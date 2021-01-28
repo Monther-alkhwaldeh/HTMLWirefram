7fac428d1b525693f0c2cb202baa543dd03bfc92
Monther-Alkhwaldeh <mmskbh.91@gmail.com>
2021-01-28 13:47:28 +0200

    add for loops and while in js page

--- jspage.js
+++ jspage.js
@@ -1,27 +1,47 @@
-var typeoflaptob=prompt("which type of laptob do you want");
-var nooflaptob=prompt("how many laptob do you want");
-var youraddres=prompt("what is your adress");
+var typeoflaptob = prompt("which type of laptob do you want toshiba or hp");
+var nooflaptob = prompt("how many laptob do you want");
+var youraddres = prompt("what is your adress");
 
 var orderdetails;
 
-if(nooflaptob >1 && nooflaptob <=3)
-{
-orderdetails="you get 10% discount";
-} else if(nooflaptob >=4 && nooflaptob <= 8){
-    orderdetails="you get 20% discount";
-} else if (nooflaptob >= 9 && nooflaptob <= 20){
-    orderdetails="you git 35% discount ";
+if (nooflaptob > 1 && nooflaptob <= 3) {
+    orderdetails = "you get 10% discount";
+} else if (nooflaptob >= 4 && nooflaptob <= 8) {
+    orderdetails = "you get 20% discount";
+} else if (nooflaptob >= 9 && nooflaptob <= 20) {
+    orderdetails = "you git 35% discount ";
 } else {
-    orderdetails="sorry no discount ";
+    orderdetails = "sorry no discount ";
 }
 
 
 document.write(orderdetails);
-document.getElementById("order").innerText=typeoflaptob;
-document.getElementById("nolaptob").innerText=nooflaptob;
-document.getElementById("youradd").innerText=youraddres;
+document.getElementById("order").innerText = typeoflaptob;
+document.getElementById("nolaptob").innerText = nooflaptob;
+document.getElementById("youradd").innerText = youraddres;
 
 
+
+
+while (typeoflaptob !== 'toshiba' && typeoflaptob !== 'hp') {
+    typeoflaptob = prompt('which type of laptob do you want toshiba or hp');
+    nooflaptob = prompt('how many laptob do you want')
+    youraddres = prompt("what is your adress");
+}
+
+var pic = '';
+if (typeoflaptob === 'hp') {
+    pic = '<img src="image/hp.jpg" /> ';
+} else if (typeoflaptob === 'toshiba') {
+    pic = '<img src="image/toshiba.jpg" />';
+}
+var result = '';
+for (var i = 0; i < nooflaptob; i++) {
+    result = result + pic;
+}
 confirm("Are you happy with you discount ");
 alert("thank you for shopping from our shop");
+
+document.write(result);
+
 
